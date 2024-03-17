f1636bb85ef6a9f212fc79715b6c2853fd2531a8
manuks-22 <manu_k_s@hotmail.com>
2024-03-17 22:38:23 +0530

    Initial WPF application.

---
+++ CurrencyToWordsApp/ViewModel/CurrencyConvertMainViewModel.cs
@@ -0,0 +1,30 @@
+﻿
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+
+namespace CurrencyToWordsApp.ViewModel
+{
+    public class CurrencyConvertMainViewModel : ViewModelBase
+    {
+        private string _amount; 
+        public ICommand SubmitCommand { get; }
+
+
+        public string Amount
+        {
+            get { return _amount; }
+            set { Set(ref _amount, value); }
+        }
+
+        public CurrencyConvertMainViewModel()
+        {
+            SubmitCommand = new RelayCommand(Submit);
+        }
+
+        private void Submit()
+        { 
+            System.Windows.MessageBox.Show($"Entered Text: {Amount}");
+        }
+    }
+}
